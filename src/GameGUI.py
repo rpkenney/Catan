187be05ddc60d636bddf35f5f9313b7ee8434b81
Robert Kenney <rpkenney@ncsu.edu>
2021-06-14 22:16:12 -0400

    added unique functionallity to tile buttons

--- src/GameGUI.py
+++ src/GameGUI.py
@@ -6,6 +6,7 @@ Created on Sun Jun 13 19:31:20 2021
 """
 
 import tkinter as tk
+import functools as partial
 
 
 class GameGUI:
@@ -45,14 +46,31 @@ class GameGUI:
         self.draw_tiles()
         
     def draw_tiles(self):
-        print(self.width)
         rows = [3, 4, 5, 4, 3]
         for tile in self.tile_graphics:
             tile.destroy()
         self.tile_graphics = []
+        c= 0
         for tile in self.tiles:
-            self.tile_graphics.append(tk.Label(self.window, text = tile))
-        
+            res = tile[0]
+            num = tile[1]
+            col = None
+            if res == 'b':
+                col = '#c4290e'
+            elif res == 'w':
+                col = '#ffff00'
+            elif res == 'l':
+                col = '#152b18'
+            elif res == 's':
+                col = '#abd685'
+            elif res == 'd':
+                col = '#e3e0a3'
+            elif res == 'r':
+                col = '#574f4f'
+            self.tile_graphics.append(tk.Button(self.window, text = num, bg = col, fg = '#0a6b5b', 
+                                                padx = 25, pady = 25, command = partial.partial(self.select_tile, c)))
+            c+=1
+            
         c = 0
         y = (self.height - 200) // 5
         for i in range(len(rows)):
@@ -60,3 +78,6 @@ class GameGUI:
             for j in range(rows[i]):
                 self.tile_graphics[c].place(anchor = 'nw', x = x * (j + 1), y = y * (i + 1))
                 c += 1
+                
+    def select_tile(self, idx):
+        print(self.tiles[idx][0])
