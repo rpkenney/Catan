27631a3cbb15f0ece6362fdb6acfd915115418f7
nwshow <noahwshowalter@gmail.com>
2021-06-15 20:04:41 -0400

    COrners and Presets complete

--- src/Corner.py
+++ src/Corner.py
@@ -6,24 +6,23 @@ parameters
     corner - int value of the corner of interest
     trueCorners - corners that are used from the square map
 """
-def get_neighbor(corner, trueCorners):
+def get_neighbor(corner, trueCorners, dim):
     width = 11
     neighbors = []
     
-    if corner % width != 0:
+    if corner % width != 10:
         neighbors.append(corner+1)
-    if corner % width != 1:
+    if corner % width != 0:
         neighbors.append(corner-1)
     
-    if corner % 2 == 0:
+    if (corner + dim) % 2 == 0:
         neighbors.append(corner-width)
     else:
         neighbors.append(corner+width)
     
-    for i in range(len(neighbors)):
-        for j in range(len(trueCorners)):
-            if neighbors[i] != trueCorners[j]:
-                neighbors.remove(i)
+    neighbors = [x for x in neighbors if x in trueCorners]
+    
+    return neighbors
         
     
     
--- src/GameManager.py
+++ src/GameManager.py
@@ -2,7 +2,19 @@ import Player
 import Tile
 import GameGUI
 import Dice
+import Corner
+import Presets
 
+true = Presets.generate_regular_hexmap(3)
+
+print(Corner.get_neighbor(27, true, 3))
+print(Corner.get_neighbor(7, true, 3))
+print(Corner.get_neighbor(63, true, 3))
+print(Corner.get_neighbor(32, true, 3))
+print(Corner.get_neighbor(33, true, 3))
+
+
+"""
 resources = ['b','r','l','s','w']
 resource_amounts = [3,3,4,4,4]
 
@@ -12,3 +24,4 @@ players = Player.generate_players(names, resources)
 tiles = Tile.generate_tiles(resources, resource_amounts)
 
 gui = GameGUI.GameGUI(tiles)
+"""
